41ae835e43c8f2568ea1758c3f7c83e92340c6ee
karrya <946302421@qq.com>
2020-06-22 17:15:58 +0800

    本地工程添加到Git

---
+++ Git-test-project/src/main/java/com/lk/git/FirstClass.java
@@ -0,0 +1,21 @@
+package com.lk.git;
+
+/**
+ * <h3>Git-test-project</h3>
+ *
+ * @author : Likai
+ * @date : 2020-06-22 16:54
+ **/
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+    }
+}
